13b4e6f97995945cb81d63d1c6ae8e2275991dd8
vc-jinal <jinal.mistry@viitor.cloud>
2023-05-29 09:14:20 +0530

    find duplicate value

---
+++ Sheet_task/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+let data = {};
+app.post('/', (req, res) => {
+    const { data: dataArray, unique } = req.body;
+
+    if (!dataArray || !Array.isArray(dataArray) || dataArray.length === 0 || !unique || !Array.isArray(unique) || unique.length === 0) {
+        return res.send({ statuscode: 400, message: 'Invalid request. Data array and unique keys are required.' });
+    }
+
+    const duplicates = {};
+    for (const item of dataArray) {
+        const {
+            sheetName,
+            ...record
+        } = item;
+        // Check if the sheetName is there or not
+        if (!sheetName) {
+            res.send({ statuscode: 400, message: "SheetName is compulsory" })
+        }
+
+        if (!data[sheetName]) {
+            data[sheetName] = [];
+        }
+
+        const duplicateItems = data[sheetName].filter(existingRecord => {
+            return unique.some(key => existingRecord[key] === record[key]);
+        });
+
+        if (duplicateItems.length > 0) {
+            duplicates[sheetName] = duplicates[sheetName] ? duplicates[sheetName].concat(item) : [...duplicateItems, item];
+        }
+        data[sheetName].push(record);
+    }
+
+    if (Object.keys(duplicates).length > 0) {
+        return res.send({ statuscode: 400, message: "Duplicate values found in unique keys.", DuplicateValue: duplicates });
+    }
+    return res.send({ statuscode: 200, message: "Data uniqueness check successful.", data });
+});
+
+app.listen(3000, () => {
+    console.log("App is running on 3000");
+});
