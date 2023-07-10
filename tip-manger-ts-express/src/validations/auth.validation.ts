d6059a6b5020c22a9040c78c5c307cc775790cbb
vc-jinal <jinal.mistry@viitor.cloud>
2023-07-10 11:42:48 +0530

    Added Validation for min max length

--- tip-manger-ts-express/src/validations/auth.validation.ts
+++ tip-manger-ts-express/src/validations/auth.validation.ts
@@ -1,20 +1,26 @@
 import Joi from "joi";
 
 export const signUpSchema = Joi.object({
-    firstName: Joi.string().required().messages({
+    firstName: Joi.string().min(3).max(10).required().messages({
         "string.empty": "First Name is not allowed to be empty",
         "any.required": "First Name is required",
+        "string.min": "First Name of min length should be 3",
+        "string.max": "First Name of max length should be 10",
     }),
-    lastName: Joi.string().required().messages({
+    lastName: Joi.string().min(3).max(10).required().messages({
         "string.empty": "Last Name is not allowed to be empty",
         "any.required": "Last Name is required",
+        "string.min": "Last Name of min length should be 3",
+        "string.max": "Last Name of max length should be 10",
     }),
     emailId: Joi.string().email().required().messages({
         "string.email": "Email must be a valid email address",
         "any.required": "Email is required",
     }),
-    password: Joi.string().required().messages({
+    password: Joi.string().min(5).max(8).required().messages({
         "any.required": "Password is required",
+        "string.min": "Password of min length should be 3",
+        "string.max": "Password of max length should be 8",
     }),
     dob: Joi.string().required().messages({
         "any.required": "dob is required",
@@ -26,7 +32,9 @@ export const signInSchema = Joi.object({
         "string.email": "Email must be a valid email address",
         "any.required": "Email is required",
     }),
-    password: Joi.string().required().messages({
+    password: Joi.string().min(5).max(8).required().messages({
         "any.required": "Password is required",
+        "string.min": "Password of min length should be 3",
+        "string.max": "Password of max length should be 8",
     }),
 });
